00cf4b19034881a187a04adb65dbd9d2e97b68e2
Aman Sheikh <haarishsheikh04@gmail.com>
2025-03-11 10:42:25 +0530

    Changes made related to tailwind

--- src/Pages/Articles/Articles.jsx
+++ src/Pages/Articles/Articles.jsx
@@ -102,19 +102,6 @@ function Articles() {
 
             <div>
 
-            {/* <button onClick={() => handleFilterChange("type", "React")} type="button" class={`${typeFilter === "React" ? "bg-gray-900 text-white" : ""} text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-10 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800`}>React</button>
-
-            <button onClick={() => handleFilterChange("type", "AI")} type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-10 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">AI</button>
-
-            <button onClick={() => handleFilterChange("type", "Computing")} type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-10 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Computing</button>
-
-            {typeFilter ? (
-                <button
-                    onClick={() => handleFilterChange("type", null)}
-                    className="text-white m-10"
-                >Clear filter</button>
-            ) : null} */}
-
         <button
             onClick={() => handleFilterChange("type", "React")}
             type="button"
